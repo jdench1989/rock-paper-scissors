1172d07fa81ca835905443f3901e06095ce57a9d
jdench1989 <jdench1989@gmail.com>
2023-03-30 19:11:56 -0600

    Implement fucntion comparing playerValue to computerValue and printing the result to console

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 let computerValue = getComputerChoice ()
 
+let playerValue = prompt('Do you pick rock, paper or scissors?')
+
 function getComputerChoice () {
 
     let a = Math.random()
 
-    console.log(a)
-
-    if (a<=1/3) { return "Rock"}
-    else if (a>1/3 && a<=(1/3)*2) { return "Paper"}
-    else { return "Scissors"}
+    if (a<=1/3) { return "rock"}
+    else if (a>1/3 && a<=(1/3)*2) { return "paper"}
+    else { return "scissors"}
 
 }
+function singleRound () {
 
-console.log(computerValue)
+    let a = playerValue.toLowerCase()
+    let b = computerValue
+
+    console.log(a)
+    console.log(b)
+
+   if (a===b) { return "It's a tie!"}
+   else if (a=='rock' && b=='paper') {return "Paper covers rock. You lose!"}
+   else if (a=='rock' && b=='scissors') { return "Rock blunts scissors. You win!"}
+   else if (a=='paper' && b=='rock') { return "Paper covers rock. You win!"}
+   else if (a=='paper' && b=='scissors') { return "Scissors cut paper. You lose!"}
+   else if (a=='scissors' && b=='rock') { return "Rock blunts scissors. You lose!"}
+   else if (a=='scissors' && b=='paper') { return "Scissors cut paper. You win"}
+   else { return 'Invalid entry. Try again.'}
+}
+console.log(singleRound())
